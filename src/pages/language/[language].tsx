99b8b09a937dd78edd6000436d972ec0b6286be2
Koni vKxni <eventbeate@gmail.com>
2023-04-08 18:37:09 +0200

    yes

--- src/pages/language/[language].tsx
+++ src/pages/language/[language].tsx
@@ -16,7 +16,7 @@ const LanguagePage: NextPage<LanguagePageProps> = ({ selectedLanguage }) => {
   );
   const [discordCopied, setDiscordCopied] = useState(false);
 
-  const imagePath = `/languages/${selectedLanguage!.name.toLowerCase()}.png`;
+  const imagePath = `/languages/${selectedLanguage!.name}.png`;
 
   const handleCopy = () => {
     navigator.clipboard.writeText(selectedLanguage?.discord || "");
